f85fac247c527a3a7b033b66906fb9713d125c2a
Ahmed Ashraf <ahmedashraf2022222@gmail.com>
2026-03-07 23:29:51 +0200

    Enhanced Input Guards

--- C Projects/Beginner Level/Age Calculator/Age_Calculator.c
+++ C Projects/Beginner Level/Age Calculator/Age_Calculator.c
@@ -22,55 +22,75 @@
 
 void Calculate_Age(Date_t Today_Date, Date_t Birth_Date, Date_t *Age)
 {
-    if (Today_Date.Day<Birth_Date.Day){
-        Today_Date.Month-=1;
-       Today_Date.Day+=Max_Days_Per_Month;
+    if (Today_Date.Day < Birth_Date.Day)
+    {
+        Today_Date.Month -= 1;
+        Today_Date.Day += Max_Days_Per_Month;
     }
-    if (Today_Date.Month<Birth_Date.Month){
-        Today_Date.Year-=1;
-        Today_Date.Month+=Max_Months_Per_Year;
+    if (Today_Date.Month < Birth_Date.Month)
+    {
+        Today_Date.Year -= 1;
+        Today_Date.Month += Max_Months_Per_Year;
     }
-    Age->Day=Today_Date.Day-Birth_Date.Day;
-    Age->Month=Today_Date.Month-Birth_Date.Month;
-    Age->Year=Today_Date.Year-Birth_Date.Year;
+    Age->Day = Today_Date.Day - Birth_Date.Day;
+    Age->Month = Today_Date.Month - Birth_Date.Month;
+    Age->Year = Today_Date.Year - Birth_Date.Year;
 }
 
 int main()
 {
+    /**
+     * @var Present_Date
+     * @brief A Date_t structure that holds the current date (day, month, year)
+     */
     Date_t Present_Date = {0};
+
+    /**
+     * @var Date_Of_Birth
+     * @brief A Date_t structure that holds the birth date of the person (day, month, year)
+     */
     Date_t Date_Of_Birth = {0};
+
+    /**
+     * @var Age_Result
+     * @brief A Date_t structure that holds the calculated age of the person (day, month, year)
+     */
+
     Date_t Age_Result = {0};
-    /*Scanning Present Date {@** */
+    
+    /*Scanning  Date.Years** */
     printf("\nEnter Today's Year: \n");
     scanf("%hd", &Present_Date.Year);
-    printf("\nEnter Today's Month: \n");
-    scanf("%hhd", &Present_Date.Month);
-    printf("\nEnter Today's Day:  \n");
-    scanf("%hhd", &Present_Date.Day);
-    /**@} */
 
-    /*Scanning Date of Birth {@** */
     printf("\nEnter Birth's Year: \n");
     scanf("%hd", &Date_Of_Birth.Year);
-    printf("\nEnter Birth's Month: \n");
-    scanf("%hhd", &Date_Of_Birth.Month);
-    printf("\nEnter Birth's Day:  \n");
-    scanf("%hhd", &Date_Of_Birth.Day);
-    /**@} */
 
-    /*Handling Edge Cases {@** */
     if (Present_Date.Year < Date_Of_Birth.Year || Date_Of_Birth.Year < Least_Valid_Year)
     {
-   
+
         printf("Wrong Year Entered Try Again");
         return Fail;
     }
-    else if (Present_Date.Month > Max_Months_Per_Year || Present_Date.Month < Zero || Date_Of_Birth.Month > Max_Months_Per_Year || Date_Of_Birth.Month < Zero)
+    /**Scanning  Date.Months */
+    printf("\nEnter Today's Month: \n");
+    scanf("%hhd", &Present_Date.Month);
+
+    printf("\nEnter Birth's Month: \n");
+    scanf("%hhd", &Date_Of_Birth.Month);
+
+    if (Present_Date.Month > Max_Months_Per_Year || Present_Date.Month < Zero || Date_Of_Birth.Month > Max_Months_Per_Year || Date_Of_Birth.Month < Zero)
     {
         printf("Wrong Month Entered Try Again");
         return Fail;
     }
-    else if (Present_Date.Day > Max_Days_Per_Month || Present_Date.Day < Zero || Date_Of_Birth.Day > Max_Days_Per_Month || Date_Of_Birth.Day < Zero)
+    /**Scanning  Date.Days */
+    printf("\nEnter Today's Day:  \n");
+    scanf("%hhd", &Present_Date.Day);
+
+    printf("\nEnter Birth's Day:  \n");
+    scanf("%hhd", &Date_Of_Birth.Day);
+
+    if (Present_Date.Day > Max_Days_Per_Month || Present_Date.Day < Zero || Date_Of_Birth.Day > Max_Days_Per_Month || Date_Of_Birth.Day < Zero)
     {
         printf("Wrong Day Entered Try Again");
         return Fail;
@@ -80,6 +100,5 @@ int main()
     Calculate_Age(Present_Date, Date_Of_Birth, &Age_Result);
 
     printf("You're %d Years , %d Months , %d Days", Age_Result.Year, Age_Result.Month, Age_Result.Day);
-
     return Success;
 }
